3ebe68e19649f5abd0d2859fd3c432025a8d6577
liraSuse <liracalero1996@gmail.com>
2024-03-19 21:07:20 -0600

    Prueba con los botones de numeros, no incuye operaciones

--- script/script.js
+++ script/script.js
@@ -7,3 +7,22 @@
     4.1 Si el numero es valido llamara la funcion indicada y borrara todo de la pantalla esperando un nuevo numero. Se repite el proceso
 5. Hay una variable que indica que ya se uso el ., antes de ingresar un valor a una variable para sumar
  */
+
+const input = document.querySelector('#item');
+const resultados2 = document.querySelector('.resultados');
+const resultados = document.querySelector('#resultados');
+let campo1='';
+let campo2='';
+
+function clean(){
+    // console.log(resultados2.textContent);
+    resultados.textContent='0';
+    campo1='';
+    campo2='';
+    console.clear();
+}
+
+function agregarNumero(numero){
+    campo1=campo1+numero;
+    console.log(campo1);
+}
